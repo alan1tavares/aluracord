e1f58478c4e85888102e1259f4759f5cd6cfe250
alan1tavares <alan1tavares@gmail.com>
2022-01-25 23:26:21 -0300

    Adicionado rota Criado a pagina chat Criado a pagina _app que nao e´ renderizada, mas e´ utilizada para colcar coisas globais pra serem utilizadas em todas as pa´ginas

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "16980c45cdfe45d8"; }
+/******/ 		__webpack_require__.h = function() { return "8e4929ce1457ab02"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,10 @@
-import appConfig from '../config.json'
-import { Box, Button, Text, TextField, Image } from '@skynexui/components'
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-
-      #__next {
-        flex: 1;
-      }
-
-      #__next > * {
-        flex: 1;
-      }
-    `}</style>
-  )
-}
+import React from 'react';
+import { useRouter } from 'next/router';
+import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import appConfig from '../config.json';
 
 function Titulo(props) {
-  const Tag = props.tag || 'h1';
+  const Tag = props.tag || 'h1'
   return (
     <>
       <Tag>{props.children}</Tag>
@@ -51,11 +20,11 @@ function Titulo(props) {
 }
 
 export default function PaginaInicial() {
-  const username = 'alan1tavares'
+  const [username, setUsername] = React.useState('omariosouto');
+  const roteamento = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex',
@@ -90,6 +59,10 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function (event){
+              event.preventDefault();
+              roteamento.push('/chat');
+            }}
             styleSheet={{
               display: 'flex',
               flexDirection: 'column',
@@ -112,6 +85,11 @@ export default function PaginaInicial() {
             </Text>
 
             <TextField
+              value={username}
+              onChange={function (event) {
+                const valor = event.target.value;
+                setUsername(valor);
+              }}
               fullWidth
               textFieldColors={{
                 neutral: {
@@ -119,7 +97,7 @@ export default function PaginaInicial() {
                   mainColor: appConfig.theme.colors.neutrals[900],
                   mainColorHighlight: appConfig.theme.colors.primary[500],
                   backgroundColor: appConfig.theme.colors.neutrals[800],
-                },
+                }
               }}
             />
             <Button
